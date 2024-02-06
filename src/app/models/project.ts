534c66e3f7c1f254530bc73ccb16f2063b18e9c3
medb2m <79202818+medb2m@users.noreply.github.com>
2024-02-06 03:22:06 +0100

    ajout des attributs des classes User, Task et Project et creation de db.json pour simuler le backend

--- src/app/models/project.ts
+++ src/app/models/project.ts
@@ -1,2 +1,10 @@
+import { Task } from "./task";
+import { User } from "./user";
+
 export class Project {
+    id !: number;
+    title !: string;
+    description !: string;
+    tasks !: Task[];
+    users !: User[];
 }
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,2 +1,5 @@
 export class User {
+    id !: number;
+    username : string = "admin"; //valeur par defaut
+    password !: string;
 }
