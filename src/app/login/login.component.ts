cbbcf369e40c58eadd77837a1a08c7e6ff42ef22
medb2m <79202818+medb2m@users.noreply.github.com>
2024-02-06 04:59:29 +0100

    ajout d'un formulaire avec des contraintes qui redirecte vers la liste des projets

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,8 +8,14 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor (private router : Router){}
 fg = new FormGroup({
-  username : new FormControl(''),
-  password : new FormControl(''),
+  username : new FormControl('',[Validators.required,Validators.minLength(3)]),
+  password : new FormControl('',[Validators.required,Validators.pattern('^[A-Z].{5,}$')]),
 })
+checkCredentials(){
+  if (this.fg.get('username')?.value == "Admin" && this.fg.get('password')?.value == "Admini" ){
+    this.router.navigate(['projects'])
+  }
+}
 }
