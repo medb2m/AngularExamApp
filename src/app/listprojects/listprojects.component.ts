68b25d929dd28ad587d6d1ad1190aa61b7accb5b
medb2m <79202818+medb2m@users.noreply.github.com>
2024-02-06 05:35:56 +0100

    ajout de la fonction fetchprojects et l'injection du service dans le composant listProjects

--- src/app/listprojects/listprojects.component.ts
+++ src/app/listprojects/listprojects.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ExamenService } from '../service/examen.service';
 
 @Component({
   selector: 'app-listprojects',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./listprojects.component.css']
 })
 export class ListprojectsComponent {
-
+constructor(private myService : ExamenService){}
+projects : any
+ngOnInit(){
+  this.myService.fetchProjects().subscribe(
+    data => {
+      console.log(data)
+      this.projects = data
+    }
+  )
+}
 }
--- src/app/service/examen.service.ts
+++ src/app/service/examen.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Project } from '../models/project';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +9,9 @@ import { Injectable } from '@angular/core';
 export class ExamenService {
 
   constructor(private http: HttpClient) { }
+
+  fetchProjects():Observable<Project[]>{
+    return this.http.get<Project[]>('http://localhost:3000/project')
+  }
+
 }
